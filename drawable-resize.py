6326e782dc5742c017d2c22189a08b4edf0814ad
karl-denby <karl.denby@gmail.com>
2016-07-14 08:33:32 +0100

    Multi OS, py 2.7/3.5 support, multiple images on cli supported

--- drawable-resize.py
+++ drawable-resize.py
@@ -1,5 +1,7 @@
-import sys, os
-from PIL import Image
+from __future__ import print_function  # in case user is using python 2.7
+from PIL import Image  # for manipulating the image
+import sys, os  # cli arguments and path functions for folder check/create
+
 
 def create_folder(folder_name):
     try:
@@ -18,7 +20,7 @@ def folder_check():
 
     for size in android_sizes:
         folder_name = android_folder + size
-        if os.path.exists(folder_name):
+        if os.path.isdir(folder_name):
             print("Folder", folder_name, "exists")
         else:
             create_folder(folder_name)
@@ -35,9 +37,10 @@ def output_images(source_image):
     }
 
     for folder in folder_to_pixel:
-        outfile = android_prefix + folder + '\\' + source_image
+        outfile = android_prefix + folder + os.path.sep + source_image
+        size = folder_to_pixel[folder]
+        print ("Write:", source_image, size, "in folder", folder)
 
-        print ("Writing", outfile, folder_to_pixel[folder])
         try:
             im = Image.open(source_image)
             im.thumbnail(folder_to_pixel[folder], Image.ANTIALIAS)
@@ -45,8 +48,11 @@ def output_images(source_image):
         except IOError:
             print ("cannot create thumbnail for '%s'" % source_image)
 
+
 # ---- Main ----
-source_image = sys.argv[1]
-print("Filename provided is", source_image)
+script_name = sys.argv[0]
+source_images = sys.argv[1:]
 folder_check()  # Check folders exist and create if needed
-output_images(source_image)  # Output Images
+
+for image in source_images:
+    output_images(image)  # Output Images
