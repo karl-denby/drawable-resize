d9f5d93eb844c18547b8ee921ab34cd4a57e9858
Karl Denby <karl.denby@gmail.com>
2016-07-22 22:29:00 +0100

    PEP8 compliant, code cleanup

--- drawable-resize.py
+++ drawable-resize.py
@@ -1,6 +1,7 @@
 from __future__ import print_function  # in case user is using python 2.7
 from PIL import Image  # for manipulating the image
-import sys, os  # cli arguments and path functions for folder check/create
+import sys  # sys
+import os  # cli arguments and path functions for folder check/create
 
 
 def create_folder(folder_name):
@@ -11,78 +12,79 @@ def create_folder(folder_name):
         print("Unable to create folder")
 
 
-def folder_check():
-    android_sizes = ['mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']
+def folder_check(folder_prefix):
+    android_sizes = ['ldpi', 'mdpi', 'hdpi', 'xhdpi', 'xxhdpi', 'xxxhdpi']
 
-
-    # check folders exist
-    android_folder = 'drawable-'
-    print("Pre-check: looking for drawable folders")
+    # check folders exist and call create code if they don't
+    print("Pre-check: looking for", folder_prefix, "folders")
 
     for size in android_sizes:
-        folder_name = android_folder + size
-        if os.path.isdir(folder_name):
-            print("Folder", folder_name, "exists")
-        else:
-            create_folder(folder_name)
-
-    android_folder = 'mipmap-'
-    print("Pre-check: looking for mipmap folders")
-
-    for size in android_sizes:
-        folder_name = android_folder + size
+        folder_name = folder_prefix + '-' + size
         if os.path.isdir(folder_name):
             print("Folder", folder_name, "exists")
         else:
             create_folder(folder_name)
 
 
-def output_images(source_image, image_type):
+def output_images(source_image, folder_prefix, image_sizes):
 
-    icon_sizes = {
-        'mdpi': (48, 48),
-        'hdpi': (72, 72),
-        'xhdpi': (96, 96),
-        'xxhdpi': (180, 180),
-        'xxxhdpi': (192, 192)
-    }
-
-    image_sizes = {
-        'mdpi': (480, 480),
-        'hdpi': (720, 720),
-        'xhdpi': (960, 960),
-        'xxhdpi': (1800, 1800),
-        'xxxhdpi': (1920, 1920)
-    }
-
-    if image_type == 'icon':
-        folder_to_pixel = icon_sizes
-        android_prefix = 'mipmap-'
-    elif image_type == 'image':
-        folder_to_pixel = image_sizes
-        android_prefix = 'drawable-'
-    else:
-        print("USAGE:", sys.argv[0], "icon|image", "filename.png")
-        exit()
-
-    for folder in folder_to_pixel:
-        outfile = android_prefix + folder + os.path.sep + source_image
-        size = folder_to_pixel[folder]
-        print ("Write:", source_image, size, "in folder", folder)
+    for resolution in image_sizes:
+        outfile = folder_prefix + '-' + resolution + os.path.sep + source_image
+        size = image_sizes[resolution]
+        print ("Write:", source_image, size, "in folder", resolution)
 
         try:
             im = Image.open(source_image)
-            im.thumbnail(folder_to_pixel[folder], Image.ANTIALIAS)
+            im.thumbnail(image_sizes[resolution], Image.ANTIALIAS)
             im.save(outfile, "PNG")
+
         except IOError:
             print ("cannot create thumbnail for '%s'" % source_image)
 
 
 # ---- Main ----
+# scriptname.py icon|image filename1.png filename2.png
 script_name = sys.argv[0]
 image_type = sys.argv[1]
 source_images = sys.argv[2:]
-folder_check()  # Check folders exist and create if needed
 
-for image in source_images:
-    output_images(image, image_type)  # Output Images
+# Sizes for mipmap launcher icons
+icon_sizes = {
+    'ldpi': (36, 36),
+    'mdpi': (48, 48),
+    'hdpi': (72, 72),
+    'xhdpi': (96, 96),
+    'xxhdpi': (144, 144),
+    'xxxhdpi': (192, 192)
+}
+
+# Sizes for general image resource
+image_sizes = {
+    'ldpi': (360, 360),
+    'mdpi': (480, 480),
+    'hdpi': (720, 720),
+    'xhdpi': (960, 960),
+    'xxhdpi': (1440, 1440),
+    'xxxhdpi': (1920, 1920)
+}
+
+# based on input provided in parameters
+# check for folders (create if needed)
+# for each image provided "2:" create an image at multiple densities
+if image_type == 'icon':
+    folder_prefix = 'mipmap'
+    image_scale = icon_sizes
+    folder_check(folder_prefix)  # Check folders exist. Create if needed.
+    for image in source_images:
+        output_images(image, folder_prefix, image_scale)  # Output Images
+
+elif image_type == 'image':
+    folder_prefix = 'drawable'
+    image_scale = image_sizes
+    folder_check(folder_prefix)  # Check folders exist. Create if needed.
+    for image in source_images:
+        output_images(image, folder_prefix, image_scale)  # Output Images
+
+# if user didn't specify icon or image then show them how the script is run
+else:
+    print("USAGE:", sys.argv[0], "icon|image", "filename.png")
